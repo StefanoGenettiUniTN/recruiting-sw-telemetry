584c08fe35d180d985ab9b57d196b836bda7035e
Stefano <stefanogenetti2000@gmail.com>
2022-09-30 18:44:48 +0200

    Statistics

--- Fsa.cpp
+++ Fsa.cpp
@@ -50,7 +50,6 @@ void Fsa::transition(char message[MAX_CAN_MESSAGE_SIZE]){
             }
             if(ID==0x0A0 && payload==0xFF01){
                 this->currentState = transitionFunction[currentState];
-                raw_log.open("../log/log.txt", ios_base::app);
                 this->create_session();
             }
             break;
@@ -58,18 +57,23 @@ void Fsa::transition(char message[MAX_CAN_MESSAGE_SIZE]){
         case Run:
             //When receive the stop message transition to Idle state
             if(ID==0x0A0 && payload==0x66FF){
+                this->store_statistics();   //create csv file with interesting session statistics
                 this->currentState = transitionFunction[currentState];
-                this->close_session();
+                this->close_session();      //close current session
                 break;
             }
+
+            //Write record in log file
             raw_log<<"("<<time(0)<<") "<<ID_string<<"#"<<payload_string<<endl;
+
+            //Update statistics increasing the number of messages with the current ID and the time when the message has been recived
+            update_statistics(ID_string);
+
             break;
     }
 }
 
 void Fsa::create_session(){
-    string raw_log_session_name;    //each session has its own log file for raw data
-
     //Create session identifier in order to uniquely identify the log file for raw data
     //Structure: [unix time]_[rand value]
     raw_log_session_name = "../log/";
@@ -84,4 +88,45 @@ void Fsa::create_session(){
 
 void Fsa::close_session(){
     raw_log.close();
+}
+
+void Fsa::update_statistics(string message_id){
+    message_identifiers.push_back(message_id);
+    number_of_messages[message_id]++;
+
+    uint64_t millisecond_time = chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now().time_since_epoch()).count();
+    mean_time[message_id].push_back(millisecond_time);
+}
+
+void Fsa::store_statistics(){
+    string statistics_file_name = raw_log_session_name+"_"+"statistics.csv";    //each session has its own statistics csv file
+
+    statistics.open(statistics_file_name);
+
+    //Write CSV header
+    statistics<<"ID,number_of_messages,mean_time"<<endl;
+
+    for(int i=0; i<message_identifiers.size(); ++i){
+        string message_ID = message_identifiers[i];
+
+        int occurences = number_of_messages[message_ID];
+
+        long double mean_time_between_ID = -1.0;
+        vector<uint64_t> timestamps = mean_time[message_ID];
+        unsigned long int time_differences_sum=0;
+
+        //If number of occurences of the given ID is greather than 1 --> compute the statistics of the elapsed time beween messages of the same ID
+        if(occurences>1){
+            for(int t=0; t<timestamps.size()-1; t++){
+                unsigned long int diff = timestamps[t+1]-timestamps[t];
+                time_differences_sum+=diff;
+            }
+            mean_time_between_ID = time_differences_sum/(1.0*(occurences-1));
+        }
+
+        statistics<<message_ID<<","<<occurences<<","<<mean_time_between_ID<<endl;
+    }
+
+
+    statistics.close();
 }
--- Fsa.h
+++ Fsa.h
@@ -5,28 +5,57 @@
 #include <fstream>
 #include <string.h>
 #include <ctime>
+#include <chrono>
+#include <vector>
+#include <map>
+
 /**
 	This class abstracts the Finite State Machine behaviour.
 	
 	Methods:
 	- Fsa(): set the initial state.
-	- getCurrentState(): get the current state of the machine. The possible states are enumerated in State.h.
-	- transition(message): given the message recived in input the machine has to perform some operations according to the current state and eventually change state
+	- getCurrentState(): 					get the current state of the machine. The possible states are enumerated in State.h.
+	- transition(message): 					given the message recived in input the machine has to perform some operations according to the current state and eventually change state
+	- create_session():						create a session when we move to the run state 
+	- close_session(): 						close the session when we go to idle from run state
+	- update_statistics(string message_id):	each time we read a message from the data flow, we update the interesting statistics of the current session
+	- store_statistics()					store the statistics in a CSV file
 
 	Attributes:
-	- currentState: current state of the machine
+	- currentState: 		current state of the machine
+	- raw_log_session_name:	each session has its own log file for raw data
+	- message_identifiers:	message IDs
+	- number_of_messages:	message ID occurences
+	- mean_time:			key: message ID ; value: vector of timestamps
+
+	- ofstream raw_log:		log file where to store raw data
+	- ofstream statistics:	file with the stored statistics
 */
 
 class Fsa
 {
 public:
 	Fsa();
+
+	ofstream raw_log;   								//log file where to store raw data
+	ofstream statistics;								//file with the stored statistics
+
 	State getCurrentState();
+	
 	void transition(char message[MAX_CAN_MESSAGE_SIZE]);
-	ofstream raw_log;   //log file where to store raw data
+
 	void create_session();
 	void close_session();
 
+	void update_statistics(string message_id);
+	void store_statistics();							//store the statistics in a CSV file
+
 private:
 	State currentState;
+
+	string raw_log_session_name;    					//each session has its own log file for raw data
+
+	vector<string> message_identifiers;					//message IDs
+	map<string, int> number_of_messages;				//message ID occurences
+	map<string, vector<uint64_t>> mean_time;			//key: message ID ; value: vector of timestamps
 };
